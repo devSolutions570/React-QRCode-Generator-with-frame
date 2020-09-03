ac1c09143e401682edc5b9e0ad5db28c6424d535
devSolution <devsolutions570@gmail.com>
2020-09-03 18:04:52 +0800

    create xlsx file with json data

--- src/App.js
+++ src/App.js
@@ -4,9 +4,87 @@ import "./App.css";
 import QrCodeGenerator from "./qrcode/QrCodeGenerator";
 
 import { CSVLink } from "react-csv";
+import * as FileSaver from "file-saver";
+import * as XLSX from "xlsx";
+import ReactExport from "react-data-export";
 
 function App() {
-  const csvData = csvjson.toCSV(
+  const ExcelFile = ReactExport.ExcelFile;
+  const ExcelSheet = ReactExport.ExcelFile.ExcelSheet;
+
+  const multiDataSet = [
+    {
+      columns: [
+        { title: "Headings", width: { wpx: 80 } }, //pixels width
+        { title: "Text Style", width: { wch: 40 } }, //char width
+        { title: "Colors", width: { wpx: 90 } },
+      ],
+      data: [
+        [
+          { value: "H1", style: { font: { sz: "24", bold: true } } },
+          { value: "Bold", style: { font: { bold: true } } },
+          {
+            value: "Red",
+            style: {
+              fill: { patternType: "solid", fgColor: { rgb: "FFFF0000" } },
+            },
+          },
+        ],
+        [
+          { value: "H2", style: { font: { sz: "18", bold: true } } },
+          { value: "underline", style: { font: { underline: true } } },
+          {
+            value: "Blue",
+            style: {
+              fill: { patternType: "solid", fgColor: { rgb: "FF0000FF" } },
+            },
+          },
+        ],
+        [
+          { value: "H3", style: { font: { sz: "14", bold: true } } },
+          { value: "italic", style: { font: { italic: true } } },
+          {
+            value: "Green",
+            style: {
+              fill: { patternType: "solid", fgColor: { rgb: "FF00FF00" } },
+            },
+          },
+        ],
+        [
+          { value: "H4", style: { font: { sz: "12", bold: true } } },
+          { value: "strike", style: { font: { strike: true } } },
+          {
+            value: "Orange",
+            style: {
+              fill: { patternType: "solid", fgColor: { rgb: "FFF86B00" } },
+            },
+          },
+        ],
+        [
+          { value: "H5", style: { font: { sz: "10.5", bold: true } } },
+          { value: "outline", style: { font: { outline: true } } },
+          {
+            value: "Yellow",
+            style: {
+              fill: { patternType: "solid", fgColor: { rgb: "FFFFFF00" } },
+            },
+          },
+        ],
+        [
+          { value: "H6", style: { font: { sz: "7.5", bold: true } } },
+          { value: "shadow", style: { font: { shadow: true } } },
+          {
+            value: "Light Blue",
+            style: {
+              fill: { patternType: "solid", fgColor: { rgb: "FFCCEEFF" } },
+            },
+          },
+        ],
+      ],
+    },
+  ];
+  // const csvData = csvjson.toCSV(
+  const csvData = [
     {
       aboutUs: "hh",
       activated: true,
@@ -41,31 +119,83 @@ function App() {
       updatedDate: "09-02-2020 7:44 AM",
       website: "hh",
     },
-    {
-      headers: "key",
-    }
-  );
-
+  ];
   const inputRef = React.useRef(null);
   const simulateClick = (e) => {
     inputRef.current.link.click();
+  };
+
+  const fileType =
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
+  const fileExtension = ".xlsx";
+
+  const exportToCSV = (csvData, fileName) => {
+    const ws = XLSX.utils.json_to_sheet(csvData);
+    var wscols = [{ wch: 6 }, { wch: 7 }, { wch: 10 }, { wch: 20 }];
+    var wsrows = [
+      { hpt: 50 }, // row 1 sets to the height of 12 in points
+      { hpx: 100 }, // row 2 sets to the height of 16 in pixels
+    ];
+    ws["!rows"] = wsrows; // ws - worksheet
+    ws["!cols"] = wscols;
+    ws["A1"].t = "s";
+    ws["A1"].s = {
+      fill: {
+        patternType: "none", // none / solid
+        fgColor: { rgb: "FF000000" },
+        bgColor: { rgb: "FFFFFFFF" },
+      },
+      font: {
+        name: "Times New Roman",
+        size: 16,
+        color: { rgb: "#FF000000" },
+        bold: true,
+        italic: false,
+        underline: false,
+      },
+      border: {
+        top: { style: "thin", color: { auto: 1 } },
+        right: { style: "thin", color: { auto: 1 } },
+        bottom: { style: "thin", color: { auto: 1 } },
+        left: { style: "thin", color: { auto: 1 } },
+      },
+    };
+    const wb = { Sheets: { data: ws }, SheetNames: ["data"] };
+    const excelBuffer = XLSX.write(wb, { bookType: "xlsx", type: "array" });
+    const data = new Blob([excelBuffer], { type: fileType });
+    FileSaver.saveAs(data, fileName + fileExtension);
   };
 
   return (
     <div className="App">
       <header className="App-header">
         <p>result: </p>
-        <CSVLink
+        {/* <CSVLink
           ref={inputRef}
           data={csvData}
           filename={"my-file.csv"}
           className="btn btn-primary"
           target="_blank"
-          style={{ display: "none" }}
+          style={{
+            display: "none",
+            border: "1px solid",
+            color: "red",
+            padding: "0px",
+            font: "20px",
+            outline: "inherit",
+            zIndex: "inherit",
+            boxShadow: "none",
+          }}
         >
           Download me
-        </CSVLink>
-        <button onClick={simulateClick}>ref butoon</button>
+        </CSVLink> */}
+        <button onClick={() => exportToCSV(csvData, "testExcel")}>
+          {/* <button onClick={() => simulateClick()}> */}
+          xlsx download butoon
+        </button>
+        <ExcelFile element={<button>Download Data With Styles</button>}>
+          <ExcelSheet dataSet={multiDataSet} name="Organization" />
+        </ExcelFile>
       </header>
     </div>
   );
